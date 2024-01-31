c0fa765dd1de45d460813cb5cc7d6e41790e35af
Esantomi <pilinnasa@gmail.com>
2024-02-01 00:36:57 +0900

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# wedding
-wedding
+## 모바일 청첩장
+원하시는 분께서는 이미지를 제외한 코드 전체를 자유롭게 활용하시기 바랍니다. 이미지는 직접 그린 것들이기 때문입니다. 
+
+웹상에서 확인할 수 있는 많은 github pages 기반 모바일 청첩장을 참고하였습니다. 특히 [AndersonChoi](https://github.com/AndersonChoi/wedding-card) 님의 모바일 청첩장이 많은 도움이 되었습니다. 감사합니다.
+
+### Libraries
+- `jQuery-Sakura`
+  - 낙화 효과
+- `simpleParallax.js`
+  - 간단한 Parallax 효과
+- `fslightbox.js`
+  - 갤러리 구현
+- `Bootstrap.js`, `Popper.js`
+  - 모달 창 구현 
+- `EmailJS`
+  - 메일 발송
---
+++ js/script.js
@@ -0,0 +1,71 @@
+// JSDoc 주석
+/**
+ * @author Esantomi <pilinnasa@gmail.com>
+ */
+
+// 낙화 효과
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+
+// 우측 버튼 비활성
+$(document).bind("contextmenu", function (e) {
+    return false;
+});
+
+
+// fslightbox를 활용한 갤러리 기능 (참고: "https://codepen.io/inpaSkyrim/pen/yLxBYNL")
+function wrap(el) {
+    const wrappingElement = document.createElement('a');
+    wrappingElement.href = el.src;
+    wrappingElement.setAttribute('data-fslightbox', 'gallery');
+    
+    // class="gallery" 내부 elements만 가져오기
+    if (el.closest('.gallery')) {
+        el.parentElement.insertBefore(wrappingElement, el);
+        wrappingElement.appendChild(el);
+    }
+}
+document.querySelectorAll('img').forEach(el => {
+    wrap(el);
+})
+// To incorporate new galleries or updates of existing ones (through adding or modifying <a> elements)
+refreshFsLightbox();
+
+
+// 선물 예약
+$(document).ready(function () {
+    $(".gift-send").click(function () {
+        $("#gift-name").text($(this).data("name"));
+    })
+
+    $("#reserveGiftButton").click(function () {
+        let name = $("#sender-name").val();
+        let message = $("#sender-message").val();
+        $("#reserveGiftButton").text("전송 중...");
+        $("#reserveGiftButton").prop("disabled", true);
+
+        emailjs.init("laZD2hrfI3vsCPBU3");
+        emailjs.send("wedding-mail", "gift_send", {
+            name: name,
+            gift: $("#gift-name").text(),
+            message: message
+        }).then(function (response) {
+            $('#giftMailModal').modal('hide');
+            alert(name + " 님의 메시지가 성공적으로 전송되었습니다.");
+
+            $("#reserveGiftButton").text("예약하기");
+            $("#sender-name").val('');
+            $("#sender-message").val('');
+            $("#reserveGiftButton").prop("disabled", true);
+        }, function (err) {
+            console.error("메시지 전송이 실패했습니다.", err);
+            alert("메시지 전송이 실패했습니다. 다시 시도해 주세요.");
+        });
+    })
+})
+
+// console log 출력
+console.log('우리 결혼합니다.');
